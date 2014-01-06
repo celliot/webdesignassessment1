62944915090b069b42ff3d871980983f12047ece
celliot <christopher.m.elliot@gmail.com>
2014-01-06 21:45:46 +1100

    cast&crew

--- hoverShow.js
+++ hoverShow.js
@@ -1,40 +1,13 @@
-var cursorX = 0; var cursorY = 0; var aX = 0; var aY = 0;
-	function currentCursorPos(e){
-		cursorX = e.pageX; cursorY = e.pageY;
-	}
-	function currentCursorPosDoc(e){
-		cursorX = event.clientX; cursorY = event.clientY;
-	}
-    if(document.all) {
-		document.onmousemove = currentCursorPosDoc;
-	}
-    else {
-		document.onmousemove = currentCursorPos;
-	}
-    function setPosition(display) {
-		if(self.pageYOffset) {
-			aX = self.pageXOffset;
-			aY = self.pageYOffset;
-		}
-		else if(document.documentElement && document.documentElement.scrollTop) {
-			aX = document.documentElement.scrollLeft;
-			aY = document.documentElement.scrollTop;
-		}
-		else if(document.body) {
-			aX = document.body.scrollLeft;
-			aY = document.body.scrollTop;
-		}
-		if(document.all) {
-			cursorX += aX;
-			cursorY += aY;
-		}
-		display.style.left = (cursorX+10) + "px";
-		display.style.top = (cursorY+10) + "px";
-    }
     function HideContent(ident) {
 		if(ident.length < 1) { return; }
 		document.getElementById(ident).style.display = "none";
 	}
+
+	function setPosition(display) {
+		display.style.left = "100px";
+		display.style.top = "10px";
+    }
+	
     function ShowContent(ident) {
 		if(ident.length < 1) { return; }
 		var tag = document.getElementById(ident);
